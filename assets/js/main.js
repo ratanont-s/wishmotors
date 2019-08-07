aa060a2c37d580a74fba04ea317dd103718dcdb3
rathanont <rathanont.s@gmail.com>
2019-08-07 17:44:23 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,33 @@
 $(function () {
     // Header
-    $('header').sticky({
-        topSpacing: 0
-    });
+    // $('header').sticky({
+    //     topSpacing: 0,
+    //     zIndex: 1020,
+    // });
 
     // Banner
     $('#banner .banner-slide').slick({
         autoplay: true,
+        autoplaySpeed: 8000,
+        speed: 3000,
+        cssEase: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)',
+        prevArrow: '<a href="#" class="slick-arrow-prev"><img src="./assets/images/icon-arrow-prev.png" class="img-fluid" /></a>',
+        nextArrow: '<a href="#" class="slick-arrow-next"><img src="./assets/images/icon-arrow-next.png" class="img-fluid" /></a>',
+    });
+
+    // Brand
+    $('#brand .brand-slide').slick({
+        autoplay: true,
+        slidesToShow: 6,
+        slidesToScroll: 4,
+        dots: true,
+        arrows: false,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+            }
+        }]
     });
 });
