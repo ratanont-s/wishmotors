5d30882bf40205ab36edf3c8d3656ecf1a782005
rathanont <rathanont.s@gmail.com>
2019-08-09 13:10:37 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,14 +5,21 @@ $(function () {
     //     zIndex: 1020,
     // });
 
+    // Toggle menu
+    $('.header-menu').hide();
+    $('#btnToggleMenu').click(function (e) {
+        e.preventDefault();
+        $('.header-menu').slideToggle();
+    });
+
     // Banner
     $('#banner .banner-slide').slick({
         autoplay: true,
         autoplaySpeed: 8000,
         speed: 3000,
         cssEase: 'cubic-bezier(0.68, -0.55, 0.265, 1.55)',
-        prevArrow: '<a href="#" class="slick-arrow-prev"><img src="./assets/images/icon-arrow-prev.png" class="img-fluid" /></a>',
-        nextArrow: '<a href="#" class="slick-arrow-next"><img src="./assets/images/icon-arrow-next.png" class="img-fluid" /></a>',
+        prevArrow: '<a href="#" class="slick-arrow-prev"></a>',
+        nextArrow: '<a href="#" class="slick-arrow-next"></a>',
     });
 
     // Brand
@@ -21,7 +28,8 @@ $(function () {
         slidesToShow: 6,
         slidesToScroll: 4,
         dots: true,
-        arrows: false,
+        appendArrows: brandSlideNav,
+        appendDots: brandSlideDots,
         responsive: [{
             breakpoint: 992,
             settings: {
@@ -36,6 +44,13 @@ $(function () {
         autoplay: true,
         slidesToShow: 3,
         dots: true,
-        arrows: false,
+        appendArrows: specialsSlideNav,
+        appendDots: specialsSlideDots,
+        responsive: [{
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 1,
+            }
+        }]
     });
 });
